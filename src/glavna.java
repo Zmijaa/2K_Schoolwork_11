f1eb509d7b50d6812a6192ea6f867c539fb35472
Zmijaa <mircevfilip98@gmail.com>
2023-12-26 13:35:39 +0100

    first commit

---
+++ src/glavna.java
@@ -0,0 +1,12 @@
+
+public class glavna {
+
+	public static void main(String[] args) {
+	  Kuce reks = new Kuce();
+	  reks.osobina();
+	  LoveckoKuce lovec = new LoveckoKuce();
+	  lovec.osobina();
+
+	}
+
+}
